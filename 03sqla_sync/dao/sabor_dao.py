de3c86ac268599d74715d0b73b4dda9b3c77c4cb
Savio Jailson Rodrigues Costa <saviojrc.1988@gmail.com>
2025-10-19 11:13:39 -0300

    Ajustes codigo

--- 03sqla_sync/dao/sabor_dao.py
+++ 03sqla_sync/dao/sabor_dao.py
@@ -1,7 +1,8 @@
 from models.sabor import Sabor
 from dao.generic_dao import GenericDAO
 from services.db_service import DBService
-
+from util.logger import Logger
+from util.helpers import formata_data
 
 ### Classe que gerencia as operações de CRUD para a entidade Sabor
 
@@ -10,12 +11,21 @@ class SaborDAO(GenericDAO):
     def __init__(self, db_service: DBService):
         super().__init__(db_service=db_service)
 
-    def consultar_por_id(self, identificador: int) -> Sabor:
+    def consultar_por_id(self, id_sabor: int) -> None:
         try:
-            sabor: Sabor = self.select_by_id(identificador, Sabor)
-            return sabor
+            with self.session as session:
+                sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).first()
+
+                if not sabor:
+                    Logger.warning(f'Sabor com ID {id_sabor} não encontrado.')
+
+                Logger.info({
+                    'id': sabor.id,
+                    'nome': sabor.nome,
+                    'data_criacao': formata_data(sabor.data_criacao)
+                })
         except Exception as e:
-            print(f'Erro ao consultar Sabor por ID {identificador}: {e}')
+            print(f'Erro ao consultar Sabor por ID {id_sabor}: {e}')
             raise e
         finally:
             self.close_session()
--- 03sqla_sync/main/select/select_sabor.py
+++ 03sqla_sync/main/select/select_sabor.py
@@ -1,28 +1,19 @@
 from util.logger import Logger
 from dao.sabor_dao import SaborDAO
 from services.db_service import DBService
-from util.helpers import formata_data
 db_service = DBService()
 sabor_dao = SaborDAO(db_service=db_service)
 
-def select_filtro_sabor(id : int):
+
+def select_filtro_sabor(id: int):
     try:
-        if not id:
-            raise ValueError("ID do sabor não fornecido.")
-
-        sabor = sabor_dao.consultar_por_id(id)
-
-        if not sabor:
-            Logger.info(f'Sabor com ID {id} não encontrado.')
-
-        Logger.info({
-            "id": sabor.id,
-            "nome": sabor.nome,
-            "data_criacao": formata_data(sabor.data_criacao)
-        })
+        sabor_dao.consultar_por_id(id_sabor=id)
     except Exception as e:
-        Logger.error(f'Erro ao selecionar sabor: {e}')
-
+        Logger.error({
+            'message': f'Erro ao consultar sabor com ID {id}',
+            'error': str(e)
+        })
+        raise e
 
 if __name__ == "__main__":
     id = int(input("Digite o ID do sabor que deseja consultar: "))
